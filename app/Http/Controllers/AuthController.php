8bb51f1a869d01e13fc77b5541e50d18157e9923
Mihailo Petric <pm130681d@student.etf.rs>
2016-05-18 17:05:16 +0200

    dodat eager loading

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
         ])){
 
             
-            return redirect('/');
+            return redirect()->intended('/');
         }
         
         return redirect('login');
