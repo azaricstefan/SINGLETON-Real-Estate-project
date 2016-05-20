932931b995444961e337d2cb5dae93b1a65af626
Mihailo Petric <pm130681d@student.etf.rs>
2016-05-20 12:33:14 +0200

    nesto od sinoc

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,14 +5,14 @@
 @endsection
 
 @section('content')
-    <div align="center">
+    <div align="center" style="">
         <form method="POST" action="{{url('login')}}">
             {{csrf_field()}}
             <label>Username</label>
             <input type="text" name="username"><br/>
             <label>Pasword</label>
-            <input type="password" name="password">
-            <input type="submit" value="Login"><br/>
+            <input type="password" name="password"><br/>
+            <input type="submit" value="Login">
             <a href="register">Register</a>
         </form>
     </div>
