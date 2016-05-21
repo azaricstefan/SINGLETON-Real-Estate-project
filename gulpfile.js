ffeeecce4207b5f381fb9da92fe302a9a42fb4ae
Stefan <azaricstefan@yahoo.com>
2016-05-21 14:26:40 +0200

    gulp.js dodat jquery i bootstrap kao dependency

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,23 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+	var dirForBower ='resources/assets/bower';
+
+	var bpath = dirForBower + '/bootstrap-sass/assets';
+	var jqueryPath = dirForBower + '/jquery';
+
+	mix.sass('app.scss')
+		.copy(jqueryPath + '/dist/jquery.min.js', 'public/js')
+		.copy(bpath + '/fonts', 'public/fonts')
+		.copy(bpath + '/javascripts/bootstrap.min.js', 'public/js')
+		.scripts([
+			'../bower/jquery/dist/jquery.min.js',
+			'../bower/bootstrap-sass/assets/javascripts/bootstrap.min.js'
+		], './public/js/merged/app.min.js'); //ovde stavi sve zajedno
 });
+
+
+
+// elixir(function(mix) {
+//     mix.sass('app.scss').version('css/app.css');
+// });
