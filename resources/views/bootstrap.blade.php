ab10bb2041c96b826987698ac584777f6a9aca0c
Stefan <azaricstefan@yahoo.com>
2016-05-21 14:38:45 +0200

    elixir upada ovde

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -5,14 +5,9 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-        <!-- jQuery library -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-
-        <!-- Latest compiled JavaScript -->
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        {{--Putem elixir-a dodato @Stefan--}}
+        {!! Html::style('css/app.css') !!}
 
         <title>@yield('title')</title>
 
@@ -40,5 +35,9 @@
         </div>
     </nav>
     @yield('scriptAfterLoad')
+    {{--Putem elixir-a dodato @Stefan--}}
+    {!! Html::script('js/jquery.min.js') !!}
+    {!! Html::script('js/bootstrap.min.js') !!}
+
     </body>
 </html>
