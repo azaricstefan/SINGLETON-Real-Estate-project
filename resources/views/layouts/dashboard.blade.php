cc59ca2df97038ab67cda629724c256d77c0c9b2
Mihailo Petric <pm130681d@student.etf.rs>
2016-05-21 12:50:47 +0200

    bisanje nepotrebnih stvari

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,16 +1,9 @@
 @extends('bootstrap')
 
-@section('navbar')
+@section('nav-bar')
     <div class="navbar-header">
         <a class="navbar-brand" href="{{url('dashboard/moderator')}}">Dashboard</a>
-    </div>{{--
-    <ul class="nav navbar-nav">
-        <li class="active"><a href="/moderator/new_ads" class="">Novi oglasi&nbsp;@if($newAdCount > 0)<span class="badge">{{$newAdCount}}</span>@endif</a></li>
-        <li><a href="/moderator/reported_comments" class="">Prijavljeni Komentari&nbsp;<span class="badge" >{{$$modDash['reportedCommentCount']}}</span></a></li>
-        <li><a href="{{url('appointments/pending')}}">Novi zakazani termini&nbsp;@if($newAppointmentCount > 0)<span class="badge">{{$newAppointmentCount}}</span>@endif</a></li>
-        <li><a href="{{url('appointments/my_appointments')}}">Moji termini&nbsp;@if($['myAppointmentCount'] > 0)<span class="badge">{{$['myAppointmentCount']}}</span>@endif</a></li>
-        <li><a href="/users">Pretraga korisnika</a></li>
-    </ul>--}}
+    </div>
 @endsection
 
 @section('content')
