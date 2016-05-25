4819cdd87a1e98058984e354a1eb8bfc7def23cf
Stefan Azarić <azaricstefan@yahoo.com>
2016-05-26 01:52:50 +0200

    Svi mogu da pretražuju nekretnine

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.bootstrap')
 
 @section('title')
-    Agencija poy
+    Agencija za nekretnine
 @endsection
 
 @section('nav-bar')
     @if(Auth::guest())
     <ul class="nav navbar-nav">
-        <li><a href="{{url('search')}}">Pretrazi oglase</a> </li>
+        <li><a href="{{url('search')}}">Pretraži oglase</a> </li>
         <li><a href="{{url('ad/create')}}">Dodaj oglas</a></li>
     </ul>
     @else
         <ul class="nav navbar-nav">
             <li><a href="{{url('dashboard')}}">Dashboard</a></li>
+            <li><a href="{{url('search')}}">Pretraži oglase</a> </li>
         </ul>
     @endif
 @endsection
