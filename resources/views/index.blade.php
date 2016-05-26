e040c3b3cc344c10eb899388a29f719a50ab8e0d
Stefan Azarić <azaricstefan@yahoo.com>
2016-05-26 02:17:42 +0200

    dodata pocetna strana link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,17 +7,19 @@
 @section('nav-bar')
     @if(Auth::guest())
     <ul class="nav navbar-nav">
-        <li><a href="{{url('search')}}">Pretraži oglase</a> </li>
-        <li><a href="{{url('ad/create')}}">Dodaj oglas</a></li>
+        <li><a href="{{url('/')}}">Početna strana</a></li>
+        <li><a href="{{url('search')}}">Pretraži oglase</a></li>
     </ul>
     @else
         <ul class="nav navbar-nav">
+            <li><a href="{{url('/')}}">Početna strana</a></li>
             <li><a href="{{url('dashboard')}}">Dashboard</a></li>
-            <li><a href="{{url('search')}}">Pretraži oglase</a> </li>
+            <li><a href="{{url('search')}}">Pretraži oglase</a></li>
         </ul>
     @endif
 @endsection
 
 @section('content')
-    {{--Ovde idu oglasi--}}
+    {{--TODO: DEFINISATI NEKU POCETNU STRANU--}}
+    <p>Ovo je početna strana. Ovde idu neki oglasi i jos neke stvari.</p>
 @endsection
