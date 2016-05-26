3e1729d881c16081a254dc0edcf02cb5e9402697
Stefan Azarić <azaricstefan@yahoo.com>
2016-05-26 14:49:56 +0200

    Dodat css (stefan)

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -7,12 +7,15 @@
 
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
         <!-- jQuery library -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
         <!-- Latest compiled JavaScript -->
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+        {{--STEFAN--}}
+        {!! Html::style('/css/styles.css') !!}
 
         <title>@yield('title')</title>
 
