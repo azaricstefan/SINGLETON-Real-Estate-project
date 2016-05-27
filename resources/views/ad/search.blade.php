0a71d846db8c0f2ec4e0fb0d22d9053f990cc438
Mihailo Petric <pm130681d@student.etf.rs>
2016-05-27 13:09:32 +0200

    Dodat link Dodaj oglas u nav-bar

--- resources/views/ad/search.blade.php
+++ resources/views/ad/search.blade.php
@@ -18,6 +18,7 @@
 		<ul class="nav navbar-nav">
 			<li><a href="{{url('/')}}">Početna strana</a></li>
             <li class="active"><a href="{{url('search')}}">Pretraži oglase</a></li>
+            <li><a href="{{url('ad/create')}}">Dodaj oglas</a></li>
             <li><a href="{{url('about')}}">Informacija o nama</a></li>
 		</ul>
 	@else
