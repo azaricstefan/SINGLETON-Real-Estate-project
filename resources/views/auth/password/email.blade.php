a778935010ee096f3160a01f713b321988255015
Mihailo Petric <pm130681d@student.etf.rs>
2016-05-28 18:31:01 +0200

    izmenjen dizajn

--- resources/views/auth/password/email.blade.php
+++ resources/views/auth/password/email.blade.php
@@ -1,9 +1,22 @@
-@extends('layouts.auth')
+@extends('layouts.bootstrap')
 
 @section('content')
     <form method="post" action="{{url('/password/email')}}">
-        {{csrf_field()}}
-        Email: <input type="email" placeholder="Vas email" name="email"><br/>
-        <input type="submit" value="Posalji mejl">
+        <div class="form-group">
+            <div class="row">
+                <div class="col-sm-5">
+                    {{--Za reklamu sa leve strane--}}
+                </div>
+                <div class="col-sm-2">
+                    {{csrf_field()}}
+                    <label class="form-control">Email: </label>
+                    <input type="email" placeholder="Vas email" name="email" class="form-control"><br/>
+                    <input type="submit" value="Posalji mejl" class="btn btn-default">
+                </div>
+                <div class="col-sm-5">
+                    {{--Za reklamu sa desne strane--}}
+                </div>
+            </div>
+        </div>
     </form>
 @endsection
